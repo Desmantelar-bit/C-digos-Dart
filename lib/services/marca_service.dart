f091b05f0a176dc836a25361b17ca844bd8924e3
Desmantelar-bit <viniciusmorales09@gmail.com>
2026-04-09 16:35:22 -0300

    Tem mais coisa

--- lib/services/marca_service.dart
+++ lib/services/marca_service.dart
@@ -4,21 +4,33 @@ import '../models/marca.dart';
 
 class MarcaService{
 
-  final String baseUrl = 'http://localhost:8080/api/marcas';
+  final String url = 'https://x8ki-letl-twmt.n7.xano.io/api:ijUECDHD/marca';
 
-  Future<List<Marca>> listar() async {
-    final resposta = await http.get(Uri.parse(baseUrl));
+
+  Future<List<Marca>> listar() async{
+    final resposta = await http.get(Uri.parse(url));
     final List listaJson = jsonDecode(resposta.body);
     return listaJson.map((item) => Marca.fromJson(item)).toList();
   }
 
-  Future<void> cadastrar(Marca marca) async {
+
+  Future<void> cadastrar(Marca marca) async{
     await http.post(
-      Uri.parse(baseUrl),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode(marca.toJson()),
+      Uri.parse(url),
+      headers: {"Content-Type" : "application/json"},
+      body: jsonEncode(marca.toJson),
     );
   }
 
+Future<void> editar(int id, Marca marca) async{
+  await http.patch(
+    Uri.parse('$url/$id'),
+    headers: {"Content-Type" : "application/json"},
+    body: jsonEncode(marca.toJson),
+  );
+}
 
+Future<void> excluir(int id) async{
+ await http.delete(Uri.parse('$url/$id'));
+}
 }
